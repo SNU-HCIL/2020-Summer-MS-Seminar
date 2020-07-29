9b25c11254d0a86f0e95b673dc549987d8eba64c
Hyeon Jeon <jeonhyun97@gmail.com>
2020-07-29 13:43:13 +0900

    basic stores setup for tic tac toe

--- hj/svelte-typescript-app/src/App.svelte
+++ hj/svelte-typescript-app/src/App.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	export let name: string;
+
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Tic Tac Toe</h1>
 </main>
 
-<style>
+<style type="text/scss"> 
 	main {
 		text-align: center;
 		padding: 1em;
@@ -16,9 +16,9 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: #79a7e8;
 		text-transform: uppercase;
-		font-size: 4em;
+		font-size: 3em;
 		font-weight: 100;
 	}
 
---
+++ hj/svelte-typescript-app/src/Board.svelte
@@ -0,0 +1,20 @@
+
+<script lang="ts">
+
+enum State {
+    E = 0, O, X      // empty, O, X
+}
+
+let cells: State[][] = [
+    [State.E, State.E, State.E],
+    [State.E, State.E, State.E],
+    [State.E, State.E, State.E]
+]
+
+
+
+
+
+</script>
+
+<div></div>
