504e1c8e9c291ce398ab177f844a9ccb5fd9fea0
Hyung-Kwon Ko <hkko@hcil.snu.ac.kr>
2020-08-02 15:43:14 +0900

    svelte+flask skeleton update

---
+++ hkko/hw2/client/README.md
@@ -0,0 +1,28 @@
+## Svelte typescript / vscode template
+
+Well-prepared svelte typescript template
+VS Code linting / diagnosis setup included
+
+### Usage
+
+**Vanilla**
+
+```
+git clone https://github.com/jeonhyun97/svelte-ts-vscode-template.git
+cd svelte-ts-vscode-template
+npm i
+```
+
+**If you want to remove my git records...**
+```
+git clone https://github.com/jeonhyun97/svelte-ts-vscode-template.git
+cd svelte-ts-vscode-template
+rm -rf .git
+rm .gitignore
+npm i
+```
+
+#### Thanks to...
+
+- [Svelte <3 TypeScript](https://svelte.dev/blog/svelte-and-typescript)
+- [pyoner/svelte-typescript/preprocess](https://github.com/pyoner/svelte-typescript/tree/master/packages/preprocess)
---
+++ hkko/hw2/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, send_from_directory
+import random
+import argparse
+
+parser = argparse.ArgumentParser(description="visualize learning curve estimation")
+parser.add_argument("--port", type=int, help="port #", default="60666")
+args = parser.parse_args()
+
+app = Flask(__name__)
+
+
+# Path for our main Svelte page
+@app.route("/")
+def base():
+    return send_from_directory('client/public', 'index.html')
+
+# Path for all the static files (compiled JS/CSS, etc.)
+@app.route("/<path:path>")
+def home(path):
+    return send_from_directory('client/public', path)
+
+@app.route("/rand")
+def hello():
+    return str(random.randint(0, 100))
+
+
+if __name__ == "__main__":
+    app.run(port=args.port, debug=True)
